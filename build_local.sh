f4fba4bd93dbb2189ea65934a141cf6fc46d7f02
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2024-01-16 10:50:46 +0000

    Remove crkbd target from build scripts

--- build_local.sh
+++ build_local.sh
@@ -6,38 +6,35 @@ rm --force *.uf2
 
 # Ensure QMK CLI is installed
 if ! command -v qmk >/dev/null 2>&1; then
-    errcho "QMK CLI not found"
-    errcho "Install with pipx install qmk"
-    exit 2
+	errcho "QMK CLI not found"
+	errcho "Install with pipx install qmk"
+	exit 2
 fi
 
 # Checkout QMK repository
 if [ -d "vial-qmk" ]; then
-    echo "VIAL QMK repository already exists, pulling latest changes"
-    cd vial-qmk
-    git pull
-    cd ..
+	echo "VIAL QMK repository already exists, pulling latest changes"
+	cd vial-qmk
+	git pull
+	cd ..
 else
-    echo "VIAL QMK repository does not exist, cloning"
-    git clone --depth 1 --recurse-submodules https://github.com/vial-kb/vial-qmk
+	echo "VIAL QMK repository does not exist, cloning"
+	git clone --depth 1 --recurse-submodules https://github.com/vial-kb/vial-qmk
 fi
 
 # Copy files to QMK repository
 if [ ! -L "vial-qmk/keyboards/crkbd/keymaps/kwmlodozeniec" ]; then
-    echo "Symlink keymaps in VIAL QMK repository"
-    ln --symbolic --force --no-dereference $(pwd)/crkbd/kwmlodozeniec vial-qmk/keyboards/crkbd/keymaps/
+	echo "Symlink keymaps in VIAL QMK repository"
+	ln --symbolic --force --no-dereference $(pwd)/crkbd/kwmlodozeniec vial-qmk/keyboards/crkbd/keymaps/
 fi
 if [ ! -L "vial-qmk/keyboards/ferris/sweep/keymaps/kwmlodozeniec" ]; then
-    echo "Symlink keymaps in VIAL QMK repository"
-    ln --symbolic --force --no-dereference $(pwd)/ferris/sweep/kwmlodozeniec vial-qmk/keyboards/ferris/sweep/keymaps/
+	echo "Symlink keymaps in VIAL QMK repository"
+	ln --symbolic --force --no-dereference $(pwd)/ferris/sweep/kwmlodozeniec vial-qmk/keyboards/ferris/sweep/keymaps/
 fi
 
 # Build firmware
 echo "Building firmware..."
 cd vial-qmk
-qmk compile -kb crkbd/r2g -km kwmlodozeniec
-qmk compile -kb crkbd/rev1 -km kwmlodozeniec
-qmk compile -kb crkbd/rev1 -km kwmlodozeniec -e CONVERT_TO=kb2040
 qmk compile -kb ferris/sweep -km kwmlodozeniec
 qmk compile -kb ferris/sweep -km kwmlodozeniec -e CONVERT_TO=kb2040
 cd ..
