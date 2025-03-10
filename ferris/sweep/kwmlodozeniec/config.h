bf0a03be5b18d028e45c765e8bcb52a778f62130
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2025-03-10 11:08:22 +0000

    Update config to match my Voyager keymap

--- ferris/sweep/kwmlodozeniec/config.h
+++ ferris/sweep/kwmlodozeniec/config.h
@@ -18,7 +18,7 @@
 #define VIAL_UNLOCK_COMBO_COLS                                                 \
   { 0, 1 }
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define VIAL_TAP_DANCE_ENTRIES 4
 #define VIAL_COMBO_ENTRIES 16
 
